07f2768d9ee14b9563ea8a0864b77c16f9e5ac53
magam <magamed773377@gmail.com>
2021-01-25 21:58:54 +0300

    Все еще начало)

--- main.py
+++ main.py
@@ -2,12 +2,36 @@ import pygame
 import sys
 
 
-def start():
-    pass
+pygame.init()
+size = WIDTH, HEIGHT = 1920, 1080
+screen = pygame.display.set_mode(size)
+FPS = 60
+clock = pygame.time.Clock()
+
+is_menu = True
 
 
-def update():
-    pass
+def terminate():    # Закрытие
+    pygame.quit()
+    sys.exit()
+
+
+def start_game():    # Начало игра (закрытие меню)
+    global is_menu
+    is_menu = False
+
+
+def start():    # Выполняется до начала игры
+    while is_menu:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+
+
+def update():    # цикл...
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
 
 
 if __name__ == '__main__':
