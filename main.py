5e251999dfdc876e2301a5b573545393aa486ff1
magam <magamed773377@gmail.com>
2021-01-25 21:52:52 +0300

    Начало

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+
+
+def start():
+    pass
+
+
+def update():
+    pass
+
+
+if __name__ == '__main__':
+    start()    # Вызов старта
+    while True:
+        update()    # цикл...
