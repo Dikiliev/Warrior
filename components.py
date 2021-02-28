2401f77b028ba96f565c36bdcc86c358e388e61a
Admin <>
2021-02-28 13:33:02 +0300

    добавил класс Particle

--- components.py
+++ components.py
@@ -1,5 +1,6 @@
 import pygame
 import general
+import random
 
 
 class Transform:    # Типа класс позиции и размера
@@ -125,6 +126,32 @@ class Animator:
 
     def update(self):
         self.current_ani.update()
+
+
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = [general.load_image("star.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(general.all_sprites)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = 500
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
 
 
 class Button(Sprite):
